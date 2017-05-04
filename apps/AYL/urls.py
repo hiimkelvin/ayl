058996e1f02060bf67dedfd8c6c819f2c736695b
Kevin <kevinma91@yahoo.com>
2017-05-03 17:38:45 -0800

    Please enter the commit message for your changes. Lines starting

--- apps/AYL/urls.py
+++ apps/AYL/urls.py
@@ -5,9 +5,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
+<<<<<<< HEAD
     url(r'^addcontentpage$', views.addcontentpage),
     url(r'^addcontent$', views.addcontent)
+=======
+    url(r'^content/(?P<id>\d+)$', views.content),
+    url(r'^login_registration$', views.loginpage),
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
+
+>>>>>>> b06db6dc1177b26b65136e0eec03a358ac3e4fb9
 ]
 
 if settings.DEBUG:
--- apps/AYL/views.py
+++ apps/AYL/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
+<<<<<<< HEAD
 from django.core.files.storage import FileSystemStorage
 from .models import User, Content, Comment, Like
 from .forms import DocumentForm
@@ -26,3 +27,56 @@ def addcontent(request):
     except:
         messages.add_message(request, messages.ERROR, 'Upload a picture!')
         return redirect('/addcontentpage')
+=======
+from django.contrib import messages
+from .models import User, Content, Comment, Like
+
+def index(request):
+    context ={
+        'all_content': Content.objects.all(),
+    }
+    return render(request, 'AYL/index.html', context)
+
+def loginpage(request):
+    return render(request, 'AYL/login.html')
+
+def register(request):
+	context = {
+		'name': request.POST['name'],
+        'username': request.POST['username'],
+        'email': request.POST['email'],
+		'password': request.POST['password'],
+		'c_pass': request.POST['c_pass'],
+	}
+	reg_results = User.objects.reg(context)
+	if reg_results['new'] != None:
+
+		request.session['user_id'] = reg_results['new'].id
+		request.session['user_name'] = reg_results['new'].name
+		return redirect('/')
+	else:
+		for error_str in reg_results['error_list']:
+			messages.add_message(request, messages.ERROR, error_str)
+		return redirect('/login_registration')
+
+def login(request):
+    context = {
+        'email': request.POST['email'],
+        'password': request.POST['password'],
+    }
+    results = User.objects.log(context)
+    if results['list_errors'] != None:
+        for error in results['list_errors']:
+            messages.add_message(request, messages.ERROR, error)
+        return redirect('/login_registration')
+    else:
+        request.session['user_id'] = results['logged_user'].id
+        request.session['user_name'] = results['logged_user'].name
+        return redirect('/')
+
+def content(request, id):
+    context ={
+        'all_content': Content.objects.all(),
+    }
+    return render(request, 'AYL/content.html', context)
+>>>>>>> b06db6dc1177b26b65136e0eec03a358ac3e4fb9
