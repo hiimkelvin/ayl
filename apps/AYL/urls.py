43cca91ea61d9399284b5d3bc1e7c902182b0ad7
Kevin <kevinma91@yahoo.com>
2017-05-03 17:32:38 -0800

    made changes to classes/added media input

--- apps/AYL/urls.py
+++ apps/AYL/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^addcontentpage$', views.addcontentpage),
+    url(r'^addcontent$', views.addcontent)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/AYL/views.py
+++ apps/AYL/views.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+from .models import User, Content, Comment, Like
+from .forms import DocumentForm
+from django.contrib import messages
 
 def index(request):
-    return render(request, 'AYL/index.html')
+    documents = Content.objects.all()
+    return render(request, 'AYL/index.html', { 'documents': documents })
+
+def addcontentpage(request):
+    return render(request, "AYL/addcontent.html")
+
+def addcontent(request):
+    try:
+        context = {
+            'title': request.POST['title'],
+            'description': request.POST['description'],
+            'document': request.FILES['document'],
+            'userID': 1
+        }
+        Content.objects.addcontent(context)
+        return redirect('/')
+    except:
+        messages.add_message(request, messages.ERROR, 'Upload a picture!')
+        return redirect('/addcontentpage')
